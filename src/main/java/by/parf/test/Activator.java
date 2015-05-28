fbca78fc601ca8324a27a21b301418af0a9d68a1
Kiryl Parfiankou <Kiryl.Parfiankou.Dev@gmail.com>
2015-05-28 13:11:34 +0300

    Added .gitignore file

--- src/main/java/by/parf/test/Activator.java
+++ src/main/java/by/parf/test/Activator.java
@@ -8,12 +8,10 @@ import org.osgi.framework.BundleContext;
  */
 public class Activator implements BundleActivator {
 
-    @Override
     public void start(BundleContext bundleContext) throws Exception {
         System.out.println("Test Bundle Activator start");
     }
 
-    @Override
     public void stop(BundleContext bundleContext) throws Exception {
         System.out.println("Test Bundle Activator stop");
     }
---
+++ src/main/java/by/parf/test/example/Example.java
@@ -0,0 +1,11 @@
+package by.parf.test.example;
+
+/**
+ * Created by parf on 26.5.15.
+ */
+public class Example {
+
+    String home() {
+        return "Hello World!";
+    }
+}
