220842c078f3f3eb5836e2b0f07eaeb559c78276
Kiryl Parfiankou <Kiryl.Parfiankou.Dev@gmail.com>
2015-06-22 23:23:53 +0300

    Update for security module

---
+++ manatee-security/src/main/java/by/parf/security/modul/Activator.java
@@ -0,0 +1,18 @@
+package by.parf.security.modul;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+/**
+ * Created by parf on 5.6.15.
+ */
+public class Activator implements BundleActivator {
+
+    public void start(BundleContext bundleContext) throws Exception {
+
+    }
+
+    public void stop(BundleContext bundleContext) throws Exception {
+
+    }
+}
